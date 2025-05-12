1438d2c09ec52b00aa44c81887eed8510b393cec
Elias Segura <eliasjsegura00@gmail.com>
2025-05-11 21:12:05 -0600

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,           // Permite acceder desde cualquier IP (0.0.0.0)
+    port: 5173,           // Puedes cambiarlo si deseas
+    strictPort: true,     // Lanza error si el puerto está ocupado
+    cors: true            // Habilita CORS por defecto
+  }
 })
